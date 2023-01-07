790ac897a093154bfe05f7cc3a8be1fa4f040938
sebastiangraba <s.graba@tiplu.de>
2023-01-07 18:19:21 +0100

    cody

--- Ludum-Dare-52/Assets/Scripts/LudumDare52/Systems/ResourceSystem.cs
+++ Ludum-Dare-52/Assets/Scripts/LudumDare52/Systems/ResourceSystem.cs
@@ -1,20 +1,19 @@
-﻿using System.Collections.Generic;
-using Amazeit.Utilities;
-using UnityEngine;
+﻿using Amazeit.Utilities;
 using LudumDare52.Crops.ScriptableObject;
+using UnityEngine;
 
 namespace LudumDare52.Systems
 {
     public class ResourceSystem : Singleton<ResourceSystem>
     {
         public Crop[] CropsList { get; private set; }
-        
+
         protected override void Awake()
         {
             base.Awake();
             AssemblyResources();
         }
-        
+
         private void AssemblyResources()
         {
             //lade scriptableobjects 
