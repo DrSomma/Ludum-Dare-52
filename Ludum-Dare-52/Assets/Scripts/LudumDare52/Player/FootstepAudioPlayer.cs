a28a03180f410a3abc814fb2acb094f5c7be4ea8
sebastiangraba <s.graba@tiplu.de>
2023-01-07 03:40:02 +0100

    add footsteps

---
+++ Ludum-Dare-52/Assets/Scripts/LudumDare52/Player/FootstepAudioPlayer.cs
@@ -0,0 +1,40 @@
+using LudumDare52.Systems;
+using UnityEngine;
+
+namespace LudumDare52.Player
+{
+    public class FootstepAudioPlayer : MonoBehaviour
+    {
+        [SerializeField]
+        private AudioClip[] foodsteps;
+
+        private float _deltaDistance;
+        private Vector2 _laspos;
+        private Vector2 _newpos;
+
+        [SerializeField]
+        private float minDis = 0.4f;
+
+        private void Start()
+        {
+            _laspos = transform.position;
+        }
+
+        private void FixedUpdate()
+        {
+            _newpos = transform.position;
+            PlayAudio();
+        }
+
+        private void PlayAudio()
+        {
+            _deltaDistance = Vector2.Distance(a: _laspos, b: _newpos);
+            if (_deltaDistance > minDis)
+            {
+                AudioClip sound = foodsteps[Random.Range(minInclusive: 0, maxExclusive: foodsteps.Length)];
+                AudioSystem.Instance.PlaySound(clip: sound, vol: 0.3f);
+                _laspos = transform.position;
+            }
+        }
+    }
+}
---
+++ Ludum-Dare-52/Assets/Scripts/LudumDare52/Player/PlayerMovement.cs
@@ -0,0 +1,57 @@
+using AmazeIt.SpriteAnimator;
+using LudumDare52.Systems.Manager;
+using UnityEngine;
+
+namespace LudumDare52.Player
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class PlayerMovement : MonoBehaviour
+    {
+       
+        [SerializeField] protected Rigidbody2D rb;
+        [SerializeField] private CharacterAnimator animator;
+        [SerializeField] protected float moveSpeed = 5f;
+        [SerializeField] private bool canMoveAxisY = true;
+        [SerializeField] private bool canMoveAxisX = true;
+
+        private Vector2 _movement;
+        private bool _canMove;
+
+        private void Start()
+        {
+            _canMove = true;
+            GameManager.Instance.OnStateUpdate += OnGameStateUpdate;
+        }
+
+        private void OnGameStateUpdate(GameState newState)
+        {
+            if (newState == GameState.Running) _canMove = true;
+        }
+
+        private void Update()
+        {
+            if(canMoveAxisX)_movement.x = Input.GetAxisRaw("Horizontal");
+            if(canMoveAxisY)_movement.y = Input.GetAxisRaw("Vertical");
+            _movement = _movement.normalized;
+            animator.SetAnimation(_movement);
+        }
+        
+
+        private void FixedUpdate()
+        {
+            if(!_canMove) return;
+
+            MovePlayer();
+        }
+
+        protected virtual void MovePlayer()
+        {
+            rb.MovePosition(rb.position + _movement.normalized * (moveSpeed * Time.fixedDeltaTime));
+        }
+
+        private void OnDestroy()
+        {
+            GameManager.Instance.OnStateUpdate -= OnGameStateUpdate;
+        }
+    }
+}
