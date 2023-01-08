2c4866ec9935304ed113f6177bd280e8b2dda1e9
sebastiangraba <s.graba@tiplu.de>
2023-01-08 19:02:31 +0100

    storage

--- Ludum-Dare-52/Assets/Scripts/LudumDare52/Storage/StorageDisplay.cs
+++ Ludum-Dare-52/Assets/Scripts/LudumDare52/Storage/StorageDisplay.cs
@@ -1,48 +1,67 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using DG.Tweening;
 using LudumDare52.Systems.Manager.PositionManager;
 using UnityEngine;
 
 namespace LudumDare52.Storage
 {
+    internal class StorageDisplayEntity
+    {
+        public IStorageable Storageable { get; set; }
+        public GameObject GameObject { get; set; }
+    }
+
     public class StorageDisplay : MonoBehaviour
     {
         [SerializeField]
         private GameObject entityContainerPrefab;
 
-        private readonly Dictionary<IStorageable, GameObject> _entitys = new();
+        // private readonly Dictionary<IStorageable, GameObject> _entitys = new();
+        private Dictionary<Vector2, StorageDisplayEntity> _slots = new();
 
         private void Start()
         {
+            _slots = StoragePositionManager.Instance.PositonList.ToDictionary<Vector2, Vector2, StorageDisplayEntity>(keySelector: x => x, elementSelector: _ => null);
             StorageManager.Instance.OnAddToStorage += OnAddToStorage;
             StorageManager.Instance.OnRemoveFromStorage += OnRemoveFromStorage;
         }
 
         private void OnRemoveFromStorage(IStorageable obj)
         {
-            GameObject entity = _entitys[obj];
-            entity.transform.DOScale(endValue: 0, duration: 0.3f).OnComplete(() => { Destroy(entity); });
-            _entitys.Remove(obj);
+            KeyValuePair<Vector2, StorageDisplayEntity> entityKeyValuePair = _slots.FirstOrDefault(x => x.Value?.Storageable == obj);
+            StorageDisplayEntity entity = entityKeyValuePair.Value;
+            if (entity == null)
+            {
+                Debug.LogError("somthing is not dispayed!");
+                return;
+            }
+
+            entity.GameObject.transform.DOScale(endValue: 0, duration: 0.3f).OnComplete(() => { Destroy(entity.GameObject); });
+            _slots[entityKeyValuePair.Key] = null;
         }
 
         private void OnAddToStorage(IStorageable obj)
         {
             Debug.Log("Display");
-            GameObject newEntity = Instantiate(entityContainerPrefab);
-            _entitys.Add(key: obj, value: newEntity);
+            GameObject newGameObjectEntity = Instantiate(entityContainerPrefab);
 
-            newEntity.transform.position = GetStoragePosInGridSpace();
-            newEntity.GetComponent<SpriteRenderer>().sprite = obj.DisplaySprite;
+            Vector2 position = GetStoragePosInGridSpace();
+
+            _slots[position] = new StorageDisplayEntity() {GameObject = newGameObjectEntity, Storageable = obj};
+
+            newGameObjectEntity.transform.position = position;
+            newGameObjectEntity.GetComponent<SpriteRenderer>().sprite = obj.DisplaySprite;
 
             Sequence sq = DOTween.Sequence();
-            sq.Append(newEntity.transform.DOScale(endValue: 0, duration: 0));
-            sq.Append(newEntity.transform.DOScale(endValue: 1, duration: 0.3f).SetEase(Ease.OutBounce));
+            sq.Append(newGameObjectEntity.transform.DOScale(endValue: 0, duration: 0));
+            sq.Append(newGameObjectEntity.transform.DOScale(endValue: 1, duration: 0.3f).SetEase(Ease.OutBounce));
         }
+
 
         private Vector2 GetStoragePosInGridSpace()
         {
-            int index = _entitys.Count - 1;
-            return StoragePositionManager.Instance.PositonList[index];
+            return StoragePositionManager.Instance.PositonList.First(x => _slots[x] == null);
         }
     }
 }
