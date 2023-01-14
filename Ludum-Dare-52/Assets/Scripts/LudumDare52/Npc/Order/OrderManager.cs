b6610a54203874ca38155ebc9ef4a35024ccaf36
sebastiangraba <s.graba@tiplu.de>
2023-01-15 00:01:59 +0100

    revert order

--- Ludum-Dare-52/Assets/Scripts/LudumDare52/Npc/Order/OrderManager.cs
+++ Ludum-Dare-52/Assets/Scripts/LudumDare52/Npc/Order/OrderManager.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using Amazeit.Utilities.Random;
 using LudumDare52.Crops.ScriptableObject;
 using LudumDare52.Progress;
@@ -9,18 +8,11 @@ using UnityEngine;
 
 namespace LudumDare52.Npc.Order
 {
-    public class DayOrder
-    {
-        
-    }
-    
+    //Todo: mach ne Factory 
     public class OrderManager : MonoBehaviour
     {
-        List<>
-
         public static Order GetNewOrder()
         {
-            Random.InitState(123);
             (int min, int max) = LevelScaleManager.Instance.GetMinMaxCrops(GameManager.Instance.Day);
             Item[] filteredList = ResourceSystem.Instance.ItemList.Where(x => Progressmanager.Instance.IsActiv(x)).ToArray();
             Item[] orderList = filteredList.Random(countMin: min, countMax: max);
